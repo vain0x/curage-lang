3b76729365f0beff3ab83892dd0b117f7f341930
vain0x <vainzerox@gmail.com>
2018-12-21 23:12:51 +0900

    chore: Config for vscode extension

---
+++ lsp/src/extension.ts
@@ -0,0 +1,11 @@
+import { ExtensionContext } from 'vscode'
+
+export function activate(context: ExtensionContext) {
+  // Something to do when the extension activated.
+  throw new Error("unimplemented")
+}
+
+export function deactivate(): Thenable<void> | undefined {
+  // Something to do when the extension is deactivating.
+  throw new Error("unimplemented")
+}
