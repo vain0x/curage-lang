2c463d3bc83a8bcff14782d9ae49cf8db4f9acd5
vain0x <vainzerox@gmail.com>
2019-01-04 18:58:09 +0900

    feat: Trivial source code validation

--- lsp/src/curage-server.ts
+++ lsp/src/curage-server.ts
@@ -2,6 +2,12 @@
 
 import {
   InitializeResult,
+  TextDocumentSyncKind,
+  DidOpenTextDocumentParams,
+  DidChangeTextDocumentParams,
+  PublishDiagnosticsParams,
+  DiagnosticSeverity,
+  Diagnostic,
 } from "vscode-languageserver-protocol"
 import {
   listenToLSPClient,
@@ -23,7 +29,19 @@ export const onMessage = (message: Message) => {
   switch (method) {
     case "initialize": {
       sendResponse(id, {
-        capabilities: {},
+        capabilities: {
+          textDocumentSync: {
+            // Indicate the server want the client to send
+            // `textDocument/didOpen` and `textDocument/didClose` notifications
+            // whenever a document opens or get closed.
+            openClose: true,
+            // Indicate the server want the client to send
+            // `textDocument/didChange` notifications
+            // whenever an open document is modified,
+            // including the full text of the modified document.
+            change: TextDocumentSyncKind.Full,
+          },
+        },
       } as InitializeResult)
       break
     }
@@ -40,7 +58,51 @@ export const onMessage = (message: Message) => {
       process.exit(0)
       break
     }
+    case "textDocument/didOpen": {
+      const { textDocument: { uri, text } } = params as DidOpenTextDocumentParams
+      validateDocument(uri, text)
+      break
+    }
+    case "textDocument/didChange": {
+      const { textDocument: { uri }, contentChanges: [{ text }] } = params as DidChangeTextDocumentParams
+      validateDocument(uri, text)
+      break
+    }
+    default: {
+      // Pass.
+      break
+    }
   }
+}
+
+/**
+ * Validates a document to publish diagnostics (warnings).
+ */
+const validateDocument = (uri: string, text: string) => {
+  const expected = `print "hello, world!"`
+
+  const diagnostics: Diagnostic[] = []
+
+  // If the text is not a hello world program, report a warning.
+  for (let i = 0; i < expected.length; i++) {
+    if (text[i] !== expected[i]) {
+      diagnostics.push({
+        message: `Expected '${expected[i]}'.`,
+        severity: DiagnosticSeverity.Warning,
+        range: {
+          start: { line: 0, character: i },
+          end: { line: 0, character: i + 1 },
+        },
+      })
+      break
+    }
+  }
+
+  // Report current diagnostics in the document identified by the `uri`.
+  sendNotify("textDocument/publishDiagnostics", {
+    uri,
+    diagnostics,
+  } as PublishDiagnosticsParams)
 }
 
 export const main = () => {
